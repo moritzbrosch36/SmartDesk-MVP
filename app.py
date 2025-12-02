5b63c15a11e7e8197f14b71e2f407dbafb5907d3
moritzbrosch36 <moritz.brosch23@googlemail.com>
2025-12-02 10:59:36 +0100

    Rename test.txt in requirements.txt. Creating models.py to as a blueprint for creating new SQL DB tables. Added app.py to create SQLite DB. clear

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from models import db, generate_models
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///smartdesk.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        generate_models() # Models erzeugen
+        db.create_all()   # Tabellen automatisch erstellen
+
+    return app
+
+if __name__ == "__main__":
+    create_app()
